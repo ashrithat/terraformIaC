b9afbe54c342a31bfb4e5530114079cc6c058872
ashrithat <138019339+ashrithat@users.noreply.github.com>
2025-03-07 18:17:19 +0530

    Update and rename file.tf to automatedcode.tf

--- automatedcode.tf
+++ automatedcode.tf
@@ -101,11 +101,12 @@ resource "aws_instance" "server1" {
 
   user_data       = <<-EOF
     #!/bin/bash
-    sudo yum install -y httpd
-    sudo systemctl start httpd
-        echo "Iam Server1" > /var/www/html/index.html
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
+    yum install -y httpd
+    systemctl enable httpd
+    echo "Iam server1" > /var/www/html/index.html
+    systemctl start httpd 
+    mkdir /var/www/html/server1/ 
+    echo "Iam server1" > /var/www/html/server1/index.html
   EOF
 }
 
@@ -118,11 +119,12 @@ resource "aws_instance" "server2" {
 
   user_data       = <<-EOF
     #!/bin/bash
-    sudo yum install -y httpd
-    sudo systemctl start httpd
-    echo "Iam Server2" > /var/www/html/index.html
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
+    yum install -y httpd
+    systemctl enable httpd
+    echo "Iam server2" > /var/www/html/index.html
+    systemctl start httpd 
+    mkdir /var/www/html/server2/ 
+    echo "Iam server2" > /var/www/html/server2/index.html
   EOF
 }
 
@@ -135,11 +137,12 @@ resource "aws_instance" "interview" {
 
   user_data       = <<-EOF
     #!/bin/bash
-    sudo yum install -y httpd
+    yum install -y httpd
+    systemctl enable httpd
     echo "Iam interview" > /var/www/html/index.html
-    sudo systemctl start httpd
-    sudo systemctl enable httpd
-     
+    systemctl start httpd
+    mkdir /var/www/html/interview/
+    echo "Iam interview" > /var/www/html/interview/index.html 
   EOF
 }
 
@@ -211,7 +214,7 @@ resource "aws_lb_target_group_attachment" "server1_attachment" {
   target_id        = aws_instance.server1.id
   port             = 80
 
-  depends_on = [aws_lb.main]  # Ensure ALB is created before attaching target group
+  depends_on = [aws_lb.main]  
 }
 
 resource "aws_lb_target_group" "server2_tg" {
@@ -236,7 +239,7 @@ resource "aws_lb_target_group_attachment" "server2_attachment" {
   target_id        = aws_instance.server2.id
   port             = 80
 
-  depends_on = [aws_lb.main]  # Ensure ALB is created before attaching target group
+  depends_on = [aws_lb.main]  
 }
 
 resource "aws_lb_target_group" "interview_tg" {
@@ -261,7 +264,7 @@ resource "aws_lb_target_group_attachment" "interview_attachment" {
   target_id        = aws_instance.interview.id
   port             = 80
 
-  depends_on = [aws_lb.main]  # Ensure ALB is created before attaching target group
+  depends_on = [aws_lb.main]  
 }
 
 # ALB
@@ -310,7 +313,7 @@ resource "aws_lb_listener_rule" "server1_rule" {
 
   condition {
     path_pattern {
-      values = ["/server1"]
+      values = ["/server1*"]
     }
   }
 
@@ -328,7 +331,7 @@ resource "aws_lb_listener_rule" "server2_rule" {
 
   condition {
     path_pattern {
-      values = ["/server2"]
+      values = ["/server2*"]
     }
   }
 
@@ -346,7 +349,7 @@ resource "aws_lb_listener_rule" "interview_rule" {
 
   condition {
     path_pattern {
-      values = ["/interview"]
+      values = ["/interview*"]
     }
   }
 
